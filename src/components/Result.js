1737562db859c54f52a458b54bcbed853ae38cf6
AlessandroDragone <alessandro.dragone@beije.it>
2022-11-23 17:29:05 +0100

    Added components

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,11 +6,61 @@ import { useMediaQuery } from "react-responsive";
 
 function Result(props) {
 
-    return(
+    const isDesktop = useMediaQuery({ minWidth: 992 })
+
+    const goToRank = () => {
+        if (!!props.callbackRank) {
+            props.callbackRank();
+        }
+    }
+
+    return (
         <View>
-            
+
+            <View style={mobile.header}>
+                <CustomButton
+                    label={'Classifica'}
+                    callback={goToRank}
+                    isDesktop={isDesktop}
+                />
+            </View>
+
+            <View>
+                <Image />
+
+                <View>
+                    <Text>Hai vinto/Hai perso</Text>
+                    <Text>Risultati</Text>
+                </View>
+            </View>
+
+            <View>
+                <CustomButton
+                    label={'Gioca ancora'}
+                />
+
+                <CustomButton
+                    label={'Torna alla home'}
+                />
+            </View>
+
         </View>
     )
 }
+
+const mobile = StyleSheet.create({
+    header: {
+        position: 'absolute',
+        top: 20,
+        right: 25,
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+    },
+});
+
+const desktop = StyleSheet.create({
+
+})
 
 export default Result
