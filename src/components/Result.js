bd906a2a6cd897c8df55dab8442c7ffd29958f75
AlessandroDragone <alessandro.dragone@beije.it>
2022-11-23 17:20:33 +0100

    Init Result

---
+++ src/components/Result.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import CustomButton from "./CustomButton";
+import { useMediaQuery } from "react-responsive";
+
+function Result(props) {
+
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
+export default Result
